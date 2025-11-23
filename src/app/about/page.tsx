00e208cf59af1b910937c938dd585b468e0c934c
wangr <wangruiyang1210@gmail.com>
2025-11-22 19:29:58 -0800

    Fix deployment error: replace <a> with Link component, add Animals to interests

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -83,17 +83,18 @@ export default function About() {
                 <span className="px-4 py-2 bg-blue-100 text-blue-700 rounded-full">Character & Logo Design</span>
                 <span className="px-4 py-2 bg-pink-100 text-pink-700 rounded-full">Software Development</span>
                 <span className="px-4 py-2 bg-green-100 text-green-700 rounded-full">Machine Learning</span>
+                <span className="px-4 py-2 bg-yellow-100 text-yellow-700 rounded-full">Animals</span>
                 <span className="px-4 py-2 bg-orange-100 text-orange-700 rounded-full">Music</span>
               </div>
             </div>
 
             <div className="text-center">
-              <a
+              <Link
                 href="/"
                 className="inline-block px-8 py-3 bg-purple-600 text-white rounded-lg font-medium hover:bg-purple-700 transition-colors"
               >
                 {t('about.backToHome')}
-              </a>
+              </Link>
             </div>
           </div>
         </div>
